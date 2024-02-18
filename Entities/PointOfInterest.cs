77361790f6e86f4e187cb84cf5290370219a003b
Z_sharper <matornyzakhar@gmail.com>
2024-02-18 21:49:30 +0300

    update

--- Entities/PointOfInterest.cs
+++ Entities/PointOfInterest.cs
@@ -8,17 +8,17 @@ namespace WebApiCourse6_7.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int PointId { get; set; }
-        
+
         [Required]
         public string PointName { get; set; }
-        
+
         [MaxLength(200)]
         public string? PointDescription { get; set; }
-        
+
         [ForeignKey("cityId")]
         public City? City { get; set; }
         public int CityId { get; set; }
-        
+
         public PointOfInterest(string name)
         {
             PointName = name;
